ba95bf670e6e3b09937ffc55782a02f6525310a8
shfshanyue <xianger94@gmail.com>
2018-11-09 12:43:19 +0800

    Add 诗词

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container">
-    {{ ping }}
+    <div class="phrase">
+      <div class="texts">
+        <div class="text" v-for="p in phrases" v-if="p" :key="p">
+          {{ p }}
+        </div>
+      </div>
+      <div class="title">
+        {{ phrase.title }} · {{ phrase.author }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,14 +19,48 @@ import { PING } from '@/query.gql'
 export default {
   data () {
     return {
-      ping: ''
+      phrase: {
+        title: '静夜思',
+        author: '李白',
+        phrase: '举头望明月，低头思故乡。'
+      }
     }
   },
   apollo: {
     ping: {
       query: PING
     }
+  },
+  computed: {
+    phrases () {
+      return this.phrase.phrase.split(/[，。！、]/)
+    }
   }
 }
 </script>
 
+<style scoped>
+.container {
+  margin: 50rpx 30rpx;
+}
+
+.phrase {
+  font-family: "楷体", "BiauKai","DFKai-SB","FZShouJinShu-S10S","FZJianZhi-M23S";
+  font-size: 1.5em;
+  writing-mode: vertical-rl;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 10px;
+}
+
+.text {
+  border-left: 1px solid red;
+}
+
+.title {
+  align-self: flex-end;
+}
+</style>

2d0dd48e3b973fbc43b55f20318707cea9f5c564
shfshanyue <xianger94@gmail.com>
2018-11-15 11:48:21 +0800

    添加字体

--- src/pages/poem/index.vue
+++ src/pages/poem/index.vue
@@ -41,6 +41,15 @@ export default {
       wx.stopPullDownRefresh()
     })
   },
+  mounted () {
+    wx.loadFontFace({
+      family: '台湾新细明体',
+      source: 'url("https://oss.xiange.tech/font/%E8%87%BA%E7%81%A3%E6%96%B0%E7%B4%B0%E6%98%8E%E9%AB%94.ttf")',
+      success (e) {
+        console.log('加载成功', e)
+      }
+    })
+  },
   methods: {
     tap (e) {
       const { windowWidth } = wx.getSystemInfoSync()
@@ -61,8 +70,9 @@ export default {
 
 .card {
   background-color: #fff;
-  font-family: "楷体", "BiauKai","DFKai-SB","FZShouJinShu-S10S","FZJianZhi-M23S";
+  font-family: "台湾新细明体", "BiauKai","DFKai-SB","FZShouJinShu-S10S","FZJianZhi-M23S";
   font-size: 1.5em;
+  line-height: 1.2em;
   writing-mode: vertical-rl;
   width: 100%;
   height: 100%;
